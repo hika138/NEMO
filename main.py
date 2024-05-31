7f32ba4f64ebe34e615b6fb7ce2b9c435e0d0342
hika138 <46370237+hika138@users.noreply.github.com>
2024-05-31 22:13:24 +0900

    雛形作成

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+class NEMO(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            intents=intents,
+            help_command=None,
+            command_prefix='!h'
+            )
+        self.initial_extensions = [
+            "cogs.gather",
+            "cogs.produce",
+            "cogs.put",
+            "cogs.remove",
+            "cogs.list",
+            "cogs.buy",
+            "cogs.trade",
+            "cogs.tell",
+            "cogs,invite"
+        ]
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+TOKEN = os.environ.get('TOKEN')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = NEMO()
+bot.run(TOKEN)
