859f6a6b640ce1e36067f897b120549034070bdb
hika138 <46370237+hika138@users.noreply.github.com>
2025-11-16 13:49:44 +0900

    refactor: 初期化コグのドキュメンテーションを改善

--- bot/cogs/initializer.py
+++ bot/cogs/initializer.py
@@ -1,7 +1,11 @@
 from discord.ext import commands
-import modules.db_utils as db_utils
-import dotenv
 
 class Initializer(commands.Cog):
+    """
+    Initializer Cog
+    
+    Args:
+        bot (commands.Bot): The bot instance
+    """
     def __init__(self, bot: commands.Bot):
         self.bot = bot
