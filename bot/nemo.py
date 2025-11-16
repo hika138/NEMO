4a396124d058fbcb492d28a942319db2898e18fd
hika138 <46370237+hika138@users.noreply.github.com>
2025-11-16 13:40:03 +0900

    fix: インポート順序を修正し、requirements.txtにpython-dotenvを追加

--- bot/nemo.py
+++ bot/nemo.py
@@ -1,10 +1,13 @@
-import discord
 import os
-from discord.ext import commands
 from os.path import join, dirname
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 class NEMO(commands.Bot):
+    """
+    NEMO Bot Class
+    """
     def __init__(self):
         super().__init__(
             intents=intents,
